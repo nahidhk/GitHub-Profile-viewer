de79568e523e87de870d3e626983d7d1d0e3ae1c
nahidhk <nahidappltd@gmail.com>
2025-01-01 22:33:53 +0600

    set robots js

--- robotsjs.main.js
+++ robotsjs.main.js
@@ -1,1 +1,26 @@
-console.log('robotsJS')
+console.log('robotsJS');
+
+function gRobotsgvn(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  document.getElementById('rbcalculator').innerHTML = result; // Set result as innerHTML
+  return result;
+}
+
+// Call the function with desired length
+gRobotsgvn(7);
+const star7 = gRobotsgvn(7);
+function uuio(){
+  gRobotsgvn(7);
+}
+function okiop() {
+  const noip = document.getElementById('outpouvaluecal').value;
+
+if (star7 === noip) {
+  alert('okip')
+}
+
+}
